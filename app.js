dbd5237b800bc4dee82f8566f27b63c19c6b47f5
Ali Misbah Malik <ali.misbah@carameltec.com>
2016-06-03 17:17:14 +0500

    * test commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
-  err.status = 406;
+  err.status = 404;
   next(err);
 });
 
